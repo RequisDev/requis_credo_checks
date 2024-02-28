268dfdb773ef713c1270852e624c62d1b28df119
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2024-02-28 14:40:23 -0500

    initial commit

---
+++ lib/requis_credo_checks.ex
@@ -0,0 +1,18 @@
+defmodule RequisCredoChecks do
+  @moduledoc """
+  Documentation for `RequisCredoChecks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RequisCredoChecks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/requis_credo_checks_test.exs
@@ -0,0 +1,8 @@
+defmodule RequisCredoChecksTest do
+  use ExUnit.Case
+  doctest RequisCredoChecks
+
+  test "greets the world" do
+    assert RequisCredoChecks.hello() == :world
+  end
+end
